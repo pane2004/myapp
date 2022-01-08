4609085a0bcfe574a80725ae91de3c743e94c511
pane2004 <alexlu2000clash@gmail.com>
2022-01-07 23:31:13 -0500

    first iteration google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCMMV6IQBjEu_wrQ7g_sVOBNxUmfn-3Vn4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/sideFunctions/depot_map.dart
+++ lib/sideFunctions/depot_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -8,15 +9,40 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Set<Marker> _markers = {};
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _markers.add(const Marker(
+          markerId: MarkerId('id-1'),
+          position: LatLng(43.65631782186271, -79.67912484606333),
+          infoWindow: InfoWindow(
+            title: "WM Mississauga Transfer Station",
+            snippet: "6465 Danville Rd, Mississauga, ON L5T 2H7",
+          )));
+      _markers.add(const Marker(
+          markerId: MarkerId('id-2'),
+          position: LatLng(43.65631782186271, -79.67912484606333),
+          infoWindow: InfoWindow(
+            title: "Disposal Bins Mississauga",
+            snippet: "1270 Central Pkwy W #404, Mississauga, ON L5C 4P4",
+          )));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Local Waste Depots"),
-      ),
-      body: const Center(
-        child: Text("This is a placeholder"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Local Waste Depots"),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          markers: _markers,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(43.592337920450426, -79.61564521970341),
+            zoom: 11,
+          ),
+        ));
   }
 }
