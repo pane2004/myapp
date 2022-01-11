3dd91c422eb2abb31aa5daffd7d829f77468af17
pane2004 <alexlu2000clash@gmail.com>
2022-01-11 12:19:43 -0500

    setup scan formatting

--- lib/topics/topic_item.dart
+++ lib/topics/topic_item.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -75,17 +77,14 @@ class TopicItem extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     ClipOval(
                       child: Material(
-                        color: Color(0xFFECFBDF),
+                        color: generateColour(scan.classification),
                         child: SizedBox(
                             width: 60,
                             height: 60,
-                            child: Icon(
-                              FontAwesomeIcons.leaf,
-                              color: Color(0xFF9ED593),
-                            )),
+                            child: generateIcon(scan.classification)),
                       ),
                     ),
                   ],
@@ -96,6 +95,53 @@ class TopicItem extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  generateIcon(String classification) {
+    var icon = classification;
+    switch (icon) {
+      case 'Recycling':
+        return Icon(
+          FontAwesomeIcons.recycle,
+          color: Color(0xFF4188D6),
+        );
+      case 'Garbage':
+        return Icon(
+          FontAwesomeIcons.trash,
+          color: Color(0xFFFFC92B),
+        );
+      case 'Compost':
+        return Icon(
+          FontAwesomeIcons.leaf,
+          color: Color(0xFF9ED593),
+        );
+      case 'Hazardous Waste':
+        return Icon(
+          FontAwesomeIcons.exclamation,
+          color: Colors.red,
+        );
+      default:
+        return Icon(
+          FontAwesomeIcons.leaf,
+          color: Color(0xFF9ED593),
+        );
+    }
+  }
+
+  generateColour(String classification) {
+    var icon = classification;
+    switch (icon) {
+      case 'Recycling':
+        return Color(0xFFEFF7FF);
+      case 'Garbage':
+        return Color(0xFFFFF8E3);
+      case 'Compost':
+        return Color(0xFFECFBDF);
+      case 'Hazardous Waste':
+        return Colors.red[100];
+      default:
+        return Color(0xFFECFBDF);
+    }
   }
 }
 
@@ -110,6 +156,10 @@ class TopicScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: const Color(0xFF84C879),
         title: Text(scan.classification),
+        shape: const ContinuousRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50))),
       ),
       body: ListView(children: [
         Hero(
@@ -117,12 +167,80 @@ class TopicScreen extends StatelessWidget {
           child:
               Image.network(scan.img, width: MediaQuery.of(context).size.width),
         ),
-        Text(
-          scan.classification,
-          style: const TextStyle(
-              height: 2, fontSize: 20, fontWeight: FontWeight.bold),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: generateIcon(scan.classification),
+              ),
+              Text(
+                scan.classification,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Divider(
+          height: 50,
+        ),
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(bottom: 15),
+                child: Text(
+                  "Before Disposing, Please Consider:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Text("1. Reusing the item"),
+              Text("2. Donating the item."),
+              Text("3. Selling the item."),
+              Text("4. Reflecting on the waste source."),
+            ],
+          ),
         ),
       ]),
     );
+  }
+
+  generateIcon(String classification) {
+    var icon = classification;
+    switch (icon) {
+      case 'Recycling':
+        return Icon(
+          FontAwesomeIcons.recycle,
+          color: Color(0xFF4188D6),
+        );
+      case 'Garbage':
+        return Icon(
+          FontAwesomeIcons.trash,
+          color: Color(0xFFFFC92B),
+        );
+      case 'Compost':
+        return Icon(
+          FontAwesomeIcons.leaf,
+          color: Color(0xFF9ED593),
+        );
+      case 'Hazardous Waste':
+        return Icon(
+          FontAwesomeIcons.exclamation,
+          color: Colors.red,
+        );
+      default:
+        return Icon(
+          FontAwesomeIcons.leaf,
+          color: Color(0xFF9ED593),
+        );
+    }
   }
 }
